2b68eb81174bfb2503ff7802d3a4c63f6a2bffba
DjCtavia <djctavia@gmail.com>
2020-05-25 23:20:35 +0200

    Fix IsLeapYear

--- class/Timestamp.c
+++ class/Timestamp.c
@@ -45,10 +45,12 @@ class Timestamp
 
     static bool IsLeapYear(int year)
     {
-        if (year % 4)
-        {
+        if (year % 400 == 0)
+            return true;
+        if (year % 100 == 0)
             return false;
-        }
-        return true;
+        if (year % 4 == 0)
+            return true;
+        return false;
     }
 };
